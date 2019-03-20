19b7e2507a37dbf483b294faf1dfca2de354c66e
Kumar <om.kumar@accenture.com>
2019-03-20 18:55:56 +0530

    latest data

---
+++ src/test/java/com/tests/NewTest2.java
@@ -0,0 +1,28 @@
+package com.tests;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NewTest2 {
+	WebDriver driver;
+	@Test
+	public void launchChrome()
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\Drivers\\chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.get("http://newtours.demoaut.com/");
+		driver.manage().window().maximize();
+		
+			
+	}
+
+}
